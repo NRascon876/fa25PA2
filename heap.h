bad28dd350b69fc4b482b1cb3404ea8e738f61d2
Nathan Rascon <nrascon8429@sdsu.edu>
2025-10-31 11:50:50 -0700

    I fixed an error in my downHeap method that was doing the wrong comparison leading to an infinite loop. Now my code should be the finished project as I am getting correct outputs when testing

--- heap.h
+++ heap.h
@@ -24,7 +24,7 @@ struct MinHeap {
     int pop(int weightArr[]) {
         // TODO: remove and return smallest index
         if (size == 0) {
-            return 0;
+            return -1;
         }
         int root = data[0];
         data[0] = data[size - 1];
@@ -53,15 +53,17 @@ struct MinHeap {
             int left = 2 * pos + 1;
             int right = 2 * pos + 2;
             int smallest = pos;
-            if (left < size && weightArr[data[left]] < weightArr[data[right]]) {
+            if (left < size && weightArr[data[left]] < weightArr[data[smallest]]) {
                 smallest = left;
             }
             if (right < size && weightArr[data[right]] < weightArr[data[smallest]]) {
                 smallest = right;
             }
-            else {
+            if (smallest == pos) {
                 return;
             }
+            swap(data[pos], data[smallest]);
+            pos = smallest;
         }
     }
 };
